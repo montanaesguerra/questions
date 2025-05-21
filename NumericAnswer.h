c45f5235b91f62a394c778f0ff6577266d6b8697
mjesguerra <mjesguerra@fbi.gov>
2025-05-21 15:56:46 -0700

    prepare NumericAnswer class

--- NumericAnswer.h
+++ NumericAnswer.h
@@ -5,4 +5,15 @@
 
 // declare NumericAnswer here
 
+class NumericAnswer: public Question {
+private:
+  double correctAnswer;
+
+public:
+  NumericAnswer(const std::string& promptStr, int value, double correct);
+
+  double scoreAnswer(const std::string& answer) const override;
+  bool validate() const override;
+};
+
 #endif
