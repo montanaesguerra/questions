3d0505e4c75809cac061a7e3a8dc5f73d29b37ab
mjesguerra <mjesguerra@fbi.gov>
2025-05-21 12:41:59 -0700

    implement Question constructor and getText()

--- Question.cpp
+++ Question.cpp
@@ -1,2 +1,24 @@
 #include "Question.h"
 #include <sstream>
+
+// Default Constructor
+Question::Question(const std::string& promptStr, int value):
+    prompt(promptStr), pointValue(value)
+{
+}
+
+// getPointValue()
+// Returns the number of points
+int Question::getPointValue() const
+{
+  return pointValue;
+}
+
+// getText()
+// Returns prompt with the point value
+std::string Question::getText() const
+{
+  std::ostringstream oss;
+  oss << prompt << " (" << pointValue << " pts)";
+  return oss.str();
+}
